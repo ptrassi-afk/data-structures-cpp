cc69ae850dbc8502ab949e2b6912dbee21fe932d
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-04-27 09:00:38 -0300

    feat: Implement Produto class with basic functionality and example usage

---
+++ exercises/03-produto/include/produto.hpp
@@ -0,0 +1,21 @@
+#ifndef PRODUTO_HPP
+#define PRODUTO_HPP
+
+#include <string>
+
+class Produto
+{
+public:
+    Produto(std::string nome, double preco, int estoque);
+    // pré-condições: preco >= 0, estoque >= 0
+    std::string nome();
+    double preco();
+    int estoque();
+
+    bool aplicarDesconto(double percentual); // 0 <= percentual <= 100
+                                             // retorna false se pré-condição não for atendida
+    bool vender(int quantidade);             // quantidade <= estoque
+                                             // retorna false se pré-condição não for atendida
+};
+
+#endif
