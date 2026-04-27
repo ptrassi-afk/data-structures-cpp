f348ff241cfb6f896dddbde5e2cd89849fa8aef9
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-04-27 10:43:42 -0300

    feat: Implement Ponto2D class with constructor and method stubs

---
+++ exercises/04-ponto2d/src/ponto2d.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include "ponto2d.hpp"
+
+Ponto2D::Ponto2D(double x, double y) : coord_x(x), coord_y(y) {}
+
+double Ponto2D::x() const
+{
+    
+}
+
+double Ponto2D::y() const {}
+double Ponto2D::distanciaOrigem() {}
+double Ponto2D::distancia(const Ponto2D &outro) {}
