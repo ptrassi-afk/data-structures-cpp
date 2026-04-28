d61ca2580257618b8b2adbb477e85b403b06aadd
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-04-28 15:24:05 -0300

    feat: Implement Sensor class with basic methods and functionality

---
+++ exercises/05-sensor/src/sensor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "sensor.hpp"
+
+Sensor::Sensor(int capacidade) : capacity(capacidade) {}
+Sensor::~Sensor() {}
+
+bool Sensor::registrar(double valor) // retorna false se capacidade esgotada
+{
+    if (this->count++ > this->capacity)
+        return false;
+
+    this->capacity = valor;
+    return true;
+}
+
+double Sensor::media() {}
+double Sensor::maximo() {}
+int Sensor::quantidade() {}
+int Sensor::capacidade() {}
