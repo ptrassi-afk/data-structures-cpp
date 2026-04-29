bc7a59f14cf27d2cd1fed8915e3acf4142cfa6ce
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-04-28 23:34:12 -0300

    feat: Add Tabuleiro class definition with member functions and constructors

---
+++ exercises/06-tabuleiro/include/tabuleiro.hpp
@@ -0,0 +1,22 @@
+#ifndef TABULEIRO_HPP
+#define TABULEIRO_HPP
+
+class Tabuleiro
+{
+private:
+    char **grid;
+    int _lines;
+    int _columns;
+
+public:
+    Tabuleiro(int linhas, int colunas);
+    ~Tabuleiro();
+
+    bool marcar(int linha, int coluna, char simbolo);
+    char simbolo(int linha, int coluna) const;
+    void imprimir();
+    int linhas();
+    int colunas();
+};
+
+#endif // TABULEIRO_H
